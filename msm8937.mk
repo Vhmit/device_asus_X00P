d573db6596c523feca0df4130d901b42df0905ec
Aoihara <veronicadisa6@gmail.com>
2020-04-05 12:37:45 +0700

    msm8937-common: SafailNet 2.0

--- msm8937.mk
+++ msm8937.mk
@@ -307,7 +307,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
