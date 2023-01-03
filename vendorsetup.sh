e0cfb8a0f34adb3340cee2ff7052244f034c42f5
Vhmit <praxedes.9630@gmail.com>
2023-01-03 14:12:08 -0300

    X00P: Use clang proton

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Clone/Fetch Upstream Device Dependencies
+# Fix kernel
+echo "clang-proton"
+git clone --depth=1 -b master https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang
+echo ""
